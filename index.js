642a654a71c17ac36be1a2bdc8d757c86b754e1d
rmadrazo97 <jmadrazo7@gmail.com>
2018-10-26 11:56:15 -0600

    new cmds
    today, version, help and default case.

--- index.js
+++ index.js
@@ -3,8 +3,34 @@ const minimist = require('minimist')
 module.exports = () => {
   const args = minimist(process.argv.slice(2))
   //defining cmd "command"
-  const cmd = args._[0];
+  //const cmd = args._[0];
+  let cmd = args._[0] || 'help'
 
+  if (args.version || args.v) {
+      cmd = 'version'
+    }
+
+  if (args.help || args.h) {
+        cmd = 'help'
+    }
+
+  switch (cmd){
+    case 'help':
+        require('./cmds/help')(args)
+        break
+
+    case 'version':
+        require('./cmds/version')(args)
+        break
+
+    case 'today':
+        require('./cmds/today')(args)
+        break
+
+    default:
+        console.error(`"${cmd}" is not a valid command!`)
+  }
+  console.log('---.---.---.---.---')
   console.log(args);
   console.log(cmd)
 }
